a9f9b511f16aae95dfc6a85281a97d3b4ae38df2
Radek Falhar <Radek.Falhar@hotmail.com>
2021-12-30 11:41:10 +0100

    Add some more output.

--- Source/YayosAmmunitionPatchMod.cs
+++ Source/YayosAmmunitionPatchMod.cs
@@ -19,26 +19,29 @@ namespace Euphoric.YayosAmmunitionPatch
             ignoredWeaponsLogMessage.AppendLine("Ignored weapons:");
 
             patchedWeaponsLogMessage.AppendLine(
-                $"Def name;Label;Mod;Ammo type;Damage def;Damage armor category;Explosion radius;Cooldown time;Warmup time;Burst shot count;Seconds between burst;Base damage;Armor penetration;Range;Accuracy touch (3);Accuracy short (12);Accuracy medium (25);Accuracy long (40);;Shots per minute;Average accuracy;Armor penetration rating;EffectiveDamage;Ammo per shot;Ammo per minute");
-            foreach (ThingDef thingDef in DefDatabase<ThingDef>.AllDefs.Where(t => t.HasComp(typeof(CompReloadable))))
+                $"Def name;Label;Mod;Ammo type;Damage def;Damage armor category;Projectile class;Explosion radius;Cooldown time;Warmup time;Burst shot count;Seconds between burst;Base damage;Armor penetration;Range;Accuracy touch (3);Accuracy short (12);Accuracy medium (25);Accuracy long (40);Forced miss radius;;Shots per minute;Average accuracy;Armor penetration rating;EffectiveDamage;Ammo per shot;Ammo per minute");
+            var reloadableThings = 
+                DefDatabase<ThingDef>.AllDefs.Where(t => t.HasComp(typeof(CompReloadable)));
+            foreach (ThingDef thingDef in reloadableThings)
             {
                 var props = thingDef.GetCompProperties<CompProperties_Reloadable>();
                 VerbProperties verb = thingDef.Verbs[0];
-                var projectile = verb?.defaultProjectile?.projectile;
-                var damageDef = projectile?.damageDef;
+                var defaultProjectile = verb?.defaultProjectile;
+                var projectileParams = defaultProjectile?.projectile;
+                var damageDef = projectileParams?.damageDef;
 
                 var ammoType = GetLocalAmmoType(props.ammoDef);
-                if (ammoType != AmmoType.Unknown && projectile != null)
+                if (ammoType != AmmoType.Unknown && projectileParams != null)
                 {
                     var secondsBetweenBurstShots = verb.ticksBetweenBurstShots / 60f;
                     var cooldownTime = thingDef.statBases.GetStatValueFromList(StatDefOf.RangedWeapon_Cooldown, 0.0f);
-                    var baseDamage = projectile.GetDamageAmount(1);
+                    var baseDamage = projectileParams.GetDamageAmount(1);
                     var accuracyTouch = thingDef.statBases.GetStatValueFromList(StatDefOf.AccuracyTouch, 0.0f);
                     var accuracyShort = thingDef.statBases.GetStatValueFromList(StatDefOf.AccuracyShort, 0.0f);
                     var accuracyMedium = thingDef.statBases.GetStatValueFromList(StatDefOf.AccuracyMedium, 0.0f);
                     var accuracyLong = thingDef.statBases.GetStatValueFromList(StatDefOf.AccuracyLong, 0.0f);
-                    var armorPenetration = projectile.GetArmorPenetration(1);
-
+                    var armorPenetration = projectileParams.GetArmorPenetration(1);
+                
                     GunParameter gunParameter = new GunParameter(
                         thingDef.defName, ammoType,
                         verb.warmupTime, cooldownTime, secondsBetweenBurstShots,
@@ -50,7 +53,7 @@ namespace Euphoric.YayosAmmunitionPatch
                         AmmoCalculation.CalculateGunAmmoParameters(gunParameter, yayoCombat.yayoCombat.maxAmmo);
 
                     patchedWeaponsLogMessage.AppendLine(
-                        $"{thingDef.defName};{thingDef.label};{thingDef.modContentPack?.Name};{props.ammoDef?.defName};{damageDef?.defName};{damageDef?.armorCategory?.defName};{projectile.explosionRadius};{cooldownTime};{verb.warmupTime};{verb.burstShotCount};{secondsBetweenBurstShots};{baseDamage};{armorPenetration};{verb.range};{accuracyTouch};{accuracyShort};{accuracyMedium};{accuracyLong};;{gunAmmoSetting.ShotsPerMinute};{gunAmmoSetting.AverageAccuracy};{gunAmmoSetting.ArmorPenetrationRating};{gunAmmoSetting.EffectiveDamage};{gunAmmoSetting.AmmoPerShot};{gunAmmoSetting.AmmoPerMinute}");
+                        $"{thingDef.defName};{thingDef.label};{thingDef.modContentPack?.Name};{props.ammoDef?.defName};{damageDef?.defName};{damageDef?.armorCategory?.defName};{defaultProjectile?.thingClass?.Name};{projectileParams.explosionRadius};{cooldownTime};{verb.warmupTime};{verb.burstShotCount};{secondsBetweenBurstShots};{baseDamage};{armorPenetration};{verb.range};{accuracyTouch};{accuracyShort};{accuracyMedium};{accuracyLong};{verb.ForcedMissRadius};;{gunAmmoSetting.ShotsPerMinute};{gunAmmoSetting.AverageAccuracy};{gunAmmoSetting.ArmorPenetrationRating};{gunAmmoSetting.EffectiveDamage};{gunAmmoSetting.AmmoPerShot};{gunAmmoSetting.AmmoPerMinute}");
                     
                     props.ammoCountPerCharge = gunAmmoSetting.AmmoPerShot;
                     props.maxCharges = gunAmmoSetting.GunShots;
@@ -58,7 +61,7 @@ namespace Euphoric.YayosAmmunitionPatch
                 else
                 {
                     ignoredWeaponsLogMessage.AppendLine(
-                        $"{thingDef.defName};{props.ammoDef?.defName};{projectile?.ToString() ?? ""}");
+                        $"{thingDef.defName};{props.ammoDef?.defName};{projectileParams?.ToString() ?? ""}");
                 }
             }
 
